4f7fed7fee557d7110b7afc78b17bfb88ec05833
osamabutt5212 <osamabutt5212@gmail.com>
2021-06-05 10:16:11 -0700

    File creation Function Created

--- app/src/main/java/com/hammadshahzad/mcsmesterproject7cardsgame/Database.java
+++ app/src/main/java/com/hammadshahzad/mcsmesterproject7cardsgame/Database.java
@@ -7,5 +7,17 @@ import java.util.Scanner;
 
 
 public class Database {
-    
+    public static void createFile(String[] args) {
+        try {
+            File myObj = new File("db.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
