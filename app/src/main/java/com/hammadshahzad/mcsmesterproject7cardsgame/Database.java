b7dcc116a478e4bf5c26a615cef4ba97979593c6
osamabutt5212 <osamabutt5212@gmail.com>
2021-06-06 10:16:11 -0700

    Writing to a file function created

--- app/src/main/java/com/hammadshahzad/mcsmesterproject7cardsgame/Database.java
+++ app/src/main/java/com/hammadshahzad/mcsmesterproject7cardsgame/Database.java
@@ -20,4 +20,15 @@ public class Database {
             e.printStackTrace();
         }
     }
+public static void writeFile(String[] args) {
+        try {
+            FileWriter myWriter = new FileWriter("db.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
